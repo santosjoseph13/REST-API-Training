9c2651717c5e90354766503f3e18e928204b23a9
santosjoseph13 <santosjoseph13@users.noreply.github.com>
2019-11-07 13:56:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express  = require('express');
 //const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 
 const attendance = express();
+mongoose.connect('mongodb://localhost/employeego');
 
 attendance.use(require('body-parser').json());
 
